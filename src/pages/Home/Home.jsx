3521162b96248daa9af7910ef3fc6f411ed2a879
amina <aminanado189@gmail.com>
2025-10-02 17:57:22 +0300

    cancle

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     return (
         <>
             <HeroSection />
-            <ChoosenSection />
+           
             <AvailibleTools />
             <BrowsingSection />
         </>
