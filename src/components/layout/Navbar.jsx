7ba47e40dec6eb14416194be2ea16239b8dc7872
sdam112 <dkaaldka@gmail.com>
2025-10-02 18:47:31 +0300

    edit navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <>
-            <Link to="/" className="p-2 text-center">الصفحة الرئيسية </Link>
+            <Link to="/" className="p-2 text-center">الصفحة الرئـيسية </Link>
             <Link to="/services" className="p-2 text-center">الخدمات </Link>
             <Link to="/contact" className="p-2 text-center"> تواصل معنا</Link>
             <Link to="/about" className="p-2 text-center">من نحن </Link>
