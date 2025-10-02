5366adb18222915eb75cd2fd181cc384b4f3e1f7
sdam112 <dkaaldka@gmail.com>
2025-10-02 18:43:11 +0300

    Edit add lorem

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -4,6 +4,7 @@ const NotFound = () => {
   return (
     <div>
       <h1>404</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam ipsa sequi ea nam reprehenderit voluptates, dignissimos quas perspiciatis vel dolorem natus et sit quae rem deleniti explicabo ut, facilis voluptas!</p>
     </div>
   )
 }
